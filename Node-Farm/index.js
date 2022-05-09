73c515da0623fa51284664874f4e5f392607b0aa
Pratik Gupta <81405739+pratik557@users.noreply.github.com>
2022-05-09 20:29:31 +0530

    Add files via upload

---
+++ Node-Farm/index.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const http = require('http'); //networking capabilities
+const url = require('url');
+////////////////////////////////////////////////////////////////////////
+
+
+//------Files--------//
+//SYNCHRONOUS/BLOCKING WAY
+//const textread = fs.readFileSync('./txt/input.txt', 'utf-8');
+//console.log(textread);
+//
+//const textwrite = `Hello Pratik This is a what we know about the avacado: ${textread}.\nCreating/Inserting javascript part ${Date.now()} `;
+//fs.writeFileSync('./txt/output.txt', textwrite);
+//console.log('File written');
+
+
+
+//ASYNCHRONOUS/NON-BLOCKING WAY
+//fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//    if (err) {
+//        return console.error("Error!!");
+//    }
+//    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//        console.log(data2);
+//
+//        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//            console.log(data3);
+//
+//
+//            fs.writeFile('.txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                console.log('Your file has been written :)');
+//            });
+//        });
+//    });
+//});
+//console.log('Will read file!')
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Server 
+
+replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    return output;
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+
+    const { query, pathname } = (url.parse(req.url, true));
+
+    //Overview Page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output);
+    }
+
+
+    //Product Page
+    else if (pathname === '/product') {
+        //console.log(query)
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+
+
+
+    //API 
+    else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.end(data);
+    }
+
+
+    //Not Found
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end("<h1>Page not found!<h1>");
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request port on 8000');
+});
